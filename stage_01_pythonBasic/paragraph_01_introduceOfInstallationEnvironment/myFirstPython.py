fa07aceb59a054fdc2d4b776779edeaaa04f2195
qihao <qihao_97@126.com>
2023-01-07 16:15:51 +0800

    First python file, for test.

---
+++ stage_01_pythonBasic/paragraph_01_introduceOfInstallationEnvironment/myFirstPython.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   myFirstPython.py    
+@Contact :   qihao_97@163.com
+@License :   (C)Copyright 2022-2023, Qihao
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/1/7 16:13    Qihao      1.0         None
+'''
+# import lib
+
+print("Hello World!")
