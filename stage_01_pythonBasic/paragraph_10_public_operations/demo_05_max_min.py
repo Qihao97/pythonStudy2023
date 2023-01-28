d030315ccaf560e98d40f090089f5287821ea5eb
qihao <qihao_97@126.com>
2023-01-28 22:40:19 +0800

    序列的公共方法，max(),min()求序列最大最小值

---
+++ stage_01_pythonBasic/paragraph_10_public_operations/demo_05_max_min.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   demo_05_max_min.py    
+@Contact :   qihao_97@163.com
+@License :   (C)Copyright 2022-2023, Qihao
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/1/28 22:36    Qihao      1.0         None
+'''
+# import lib
+
+"""
+max():返回容器中元素最大值
+    max(目标序列)
+min():返回容器中元素最小值
+    min(目标序列)
+"""
+
+str1 = "abcdefg"
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(max(str1))
+
+print(min(list1))
+
