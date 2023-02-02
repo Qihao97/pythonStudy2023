a06700ce9710e0deebcd3dd8d9a8ab2da9002d16
qihao <qihao_97@126.com>
2023-02-02 23:38:27 +0800

    函数基础

---
+++ stage_01_pythonBasic/paragraph_12_function_basic/demo_01_function.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   demo_01_function.py    
+@Contact :   qihao_97@163.com
+@License :   (C)Copyright 2022-2023, Qihao
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/2/2 23:32    Qihao      1.0         None
+'''
+# import lib
+"""
+函数体验
+定义函数：
+def 函数名(参数):
+    函数体
+    ...
+
+调用函数：
+函数名(参数)
+
+函数需要先定义再使用，根据实际情况，参数可有可无
+"""
+
+# 复现ATM机器取钱
+# 1.搭建整体框架
+
+# 2.确定选择功能页面
+
