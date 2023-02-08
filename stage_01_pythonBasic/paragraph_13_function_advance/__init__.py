d81255175aa2188c5475448b4998a7069c2e6430
qihao <qihao_97@126.com>
2023-02-08 23:29:00 +0800

    变量的作用域：局部变量和全局变量

---
+++ stage_01_pythonBasic/paragraph_13_function_advance/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py.py    
+@Contact :   qihao_97@163.com
+@License :   (C)Copyright 2022-2023, Qihao
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/2/8 21:24    Qihao      1.0         None
+'''
+# import lib
---
+++ stage_01_pythonBasic/paragraph_13_function_advance/demo_01_variate_scope.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   demo_01_variate_scope.py    
+@Contact :   qihao_97@163.com
+@License :   (C)Copyright 2022-2023, Qihao
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/2/8 23:21    Qihao      1.0         None
+'''
+# import lib
+"""
+变量作用域：变量作用域指的是变量生效的范围：主要包括局部变量和全局变量
+局部变量：定义在函数体内部的变量，只在函数体内部生效
+作用：在函数体内部，临时保存数据，当函数调用完毕后，局部变量即销毁
+"""
+
