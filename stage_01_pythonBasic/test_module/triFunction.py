97e715197b3a2c9c94f926006e9556137930d426
Qihao <qihao_97@163.com>
2023-07-17 21:18:31 +0800

    三角函数

---
+++ stage_01_pythonBasic/test_module/triFunction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   triFunction.py    
+@Contact :   qihao_97@163.com
+@License :   (C)Copyright 2022-2023, Qihao
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/7/17 21:16    Qihao      1.0         None
+'''
+# import lib
+
+import math
+x = 1
+result = math.sin(x)
+print(result)
